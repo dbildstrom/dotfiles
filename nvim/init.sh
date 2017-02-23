bab72b823f405eeb7300881da69e85642b4ec775
David Bildström <bildstrom@gmail.com>
2017-02-23 13:01:58 +0100

    Use local install of eslint when needed

--- nvim/init.sh
+++ nvim/init.sh
@@ -3,5 +3,3 @@ SCRIPT_PATH="`realpath ${BASH_SOURCE[0]}`"
 SCRIPT_DIR="`dirname "${SCRIPT_PATH}"`"
 (cd "$SCRIPT_DIR"; mkdir -p {backup_files,swap_files,undo_files})
 (cd "$SCRIPT_DIR"; nvim -u init_plugins.vim)
-(cd "$SCRIPT_DIR"; npm install)
-echo "Add node_modules/.bin/eslint to your path"
